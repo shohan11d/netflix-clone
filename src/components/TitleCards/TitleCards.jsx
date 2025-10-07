f1c75788687ebe18ef7c44c2622528c89d0ac0ce
shohan11d <shohan11d@gmail.com>
2025-10-07 16:44:26 +0600

    card-list added

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,7 +1,29 @@
-import 'TitleCards.css'
+import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data'
+import { useEffect, useRef } from 'react'
+
+
+
 function TitleCards() {
+    const cardsRef = useRef();
+const handleWheel = (e) => {
+    e.preventDefault();
+    cardsRef.current.scrollLeft += e.deltaY;
+}
+useEffect(() => {
+  cardsRef.current.addEventListener('wheel', handleWheel)});
     return (
-        <div>
+        <div className="title-cards">
+            <h2>Popular on Netflix</h2>
+            <div className="card-list" ref={cardsRef}>
+                {cards_data.map((card,index)=>{
+                    return <div className="card" key={index}>
+                        <img src={card.image} alt={card.title}/>
+                        <p>{card.name}</p>
+
+                    </div>
+                })}
+            </div>
             
         </div>
     )
