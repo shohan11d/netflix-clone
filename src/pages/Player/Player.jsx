bf381e6de05b3310b64a134281cb8872b9615ed0
shohan11d <shohan11d@gmail.com>
2025-10-14 09:48:05 +0600

    player added, bugged doesn't work properly

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,10 +1,54 @@
-import 'Player.css'
+import "./Player.css";
+import back_arrow_icon from "../../assets/back_arrow_icon.png";
+import play_icon from "../../assets/play_icon.png";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+// Bugged
 function Player() {
-    return (
-        <div>
-            
-        </div>
+  const { id } = useParams();
+  const [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    typeof: "",
+  });
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNWY5MTEzNTkzYTU0ZDM5YjJlY2M5NTY4MDM2OGRjNyIsIm5iZiI6MTcyODM4NDAzMy44NjksInN1YiI6IjY3MDUwYzIxY2NiYjRlN2YxNjY5Y2NhMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lSg0SIDcq-s9y-NOb5X5LplTrWmJpCwgcIVwKJanW6c",
+    },
+  };
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`,
+      options
     )
+      .then((res) => res.json())
+      .then((res) => setApiData(res.results[0]))
+      .catch((err) => console.log(err));
+  }, [id]);
+
+  return (
+    <div className="player">
+      <img src={back_arrow_icon} alt="" />
+      <iframe
+        src="https://www.youtube.com/embed/4vtSa3m7PJs"
+        title="How to choose a Netflix Streaming Plan | Netflix"
+        frameborder="0"
+        allowfullscreen
+      ></iframe>
+      <div className="player-info">
+        <p>{apiData.published_at.slice(0, 10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
+    </div>
+  );
 }
 
-export default Player
+export default Player;
